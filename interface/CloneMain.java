6eed5a01aa40048d4442326e7b083979cbc654dc
Vishal Lalwani <Lalwani_v@India.XoriantCorp.com>
2016-10-18 17:18:57 +0530

    clone added

--- interface/CloneMain.java
+++ interface/CloneMain.java
@@ -1,33 +1,78 @@
-import java.util.*;
-class Employee implements Cloneable
-	{
-		private int day,month,year;
-		private String name;
+public class CloneMain {
+    public static void main(String[] args) throws Exception {
 
-		public Employee(int day,int month,int year,String name)
-		{
-		  this.day=day;
-		  this.month=month;
-		  this.year=year;
-		  this.name=name;
-		}
-	public Object clone() {
-	try {
-	  return super.clone();
-	} catch (CloneNotSupportedException e) {
-	  return null;
-	}
-  }
-	}
-	
-	
-public class CloneMain 
-{
-	public static void main(String[] args)
-	{
-		Employee e=new Employee(19,03,1994,"vishal");
-		Employee e1=(Employee)e.clone();
-		System.out.println("e hashcode"+e.hashCode());
-		System.out.println("e1 hashcode"+e1.hashCode());
-	}
+        Employee e1 = new Employee(1, new Date(1,2,1994));
+        Employee e2 = e1.clone();
+
+        System.out.println(e1.hashCode());
+        System.out.println(e2.hashCode());
+
+        EmployeeDeep ed1 = new EmployeeDeep(1, new Date(1,2,1994));
+        EmployeeDeep ed2 = ed1.clone();
+
+        System.out.println(ed1.hashCode());
+        System.out.println(ed2.hashCode());
+
+    }
+}
+
+class Employee implements Cloneable {
+
+    private int empID;
+    private Date dateOfBirth;
+
+    public Employee(int empID, Date dateOfBirth){
+        this.empID = empID;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String toString(){
+        return empID + " : " + dateOfBirth;
+    }
+
+    public Employee clone() throws CloneNotSupportedException {
+        return (Employee)super.clone();
+    }
+}
+
+class EmployeeDeep implements Cloneable {
+
+    private int empID;
+    private Date dateOfBirth;
+
+    public EmployeeDeep(int empID, Date dateOfBirth){
+        this.empID = empID;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String toString(){
+        return empID + " : " + dateOfBirth;
+    }
+
+    public EmployeeDeep clone() throws CloneNotSupportedException {
+        EmployeeDeep ed = (EmployeeDeep)super.clone();
+        ed.dateOfBirth = (Date) dateOfBirth.clone();
+        return ed;
+    }
+}
+
+class Date implements Cloneable {
+
+    private int date;
+    private int month;
+    private int year;
+
+    public Date(int date, int month, int year){
+        this.date = date;
+        this.month = month;
+        this.year = year;
+    }
+
+    public String toString(){
+        return date + "/" + month + "/" + year;
+    }
+
+    public Date clone() throws CloneNotSupportedException {
+        return (Date) super.clone();
+    }
 }
